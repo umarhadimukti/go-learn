2747629c974db8e88a130374fd0601dd7f8a51db
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-11-19 10:50:41 +0700

    learn(basic): operator new in go

---
+++ 16-go-operator-new/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Product struct {
+	Name, Category string
+	Price string
+}
+
+func main() {
+	var product1 *Product = new(Product) // create empty pointer using 'new' keyword
+	var product2 *Product = product1
+	product2 = &Product{"Acer Predator", "Laptop", "Rp 20.000.000"}
+	fmt.Println(*product1)
+	fmt.Println(*product2)
+}

47d626a825f8e48ed13f793cddd02679c1cfec7f
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-11-19 11:19:06 +0700

    learn(basic): pointer function in go

---
+++ 17-go-pointer-function/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Product struct {
+	Name, Category string
+	Price          float32
+}
+
+func discount(product *Product, percentage float32) {
+	product.Price = product.Price * percentage
+}
+
+func main() {
+	var product1 Product = Product{"Acer Predator", "Laptop", 20000000}
+	discount(&product1, 0.5)
+	fmt.Println("--- Product 1 ---")
+	fmt.Println("Product Name:", product1.Name)
+	fmt.Println("Category:", product1.Category)
+	fmt.Println("Price:", "Rp" + fmt.Sprint(int(product1.Price)))
+}

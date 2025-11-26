81d2eba1b49416ff264bc79a441a564ccab82001
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-11-26 16:18:50 +0700

    feat: add TestMathTable testing (table test)

--- tests/math_test.go
+++ tests/math_test.go
@@ -1,11 +1,13 @@
 package tests
 
 import (
-	"testing"
 	"fmt"
+	_ "math"
+	"runtime"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	_"math"
 	math_pkg "github.com/umarhadimukti/go-learn/go-std-libraries/math"
 )
 
@@ -34,4 +36,81 @@ func TestIsNan(t *testing.T) {
 	result := math_pkg.IsNan(4)
 	require.Equal(t, true, result, "expected true but got false")
 	fmt.Println("TestIsNan passed")
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Skipping test on macOS")
+	}
+	result := math_pkg.Power(3, 3)
+	require.Equal(t, float64(27), result, "expected 27 but got different value")
+	fmt.Println("TestSkip passed")
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("Starting tests...")
+	m.Run()
+	fmt.Println("Tests completed.")
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Should be test math is nan", func(t *testing.T) {
+		result := math_pkg.IsNan(0.4)
+		require.Equal(t, false, result, "expected false but got true")
+		fmt.Println("SubTest IsNan passed")
+	})
+
+	t.Run("Should be test math power", func(t *testing.T) {
+		result := math_pkg.Power(10, 2)
+		require.Equal(t, float64(100), result, fmt.Sprintf("expected 100 but actual value is %v", result))
+		fmt.Println("SubTest Power passed")
+	})
+}
+
+func TestMathTable(t *testing.T) {
+	// declare tests array of struct
+	tests := []struct {
+		name string
+		request float64
+		expected float64
+		option string
+	} {
+		{
+			name: "Test Square Root of 49",
+			request: 49,
+			expected: 7,
+			option: "sqrt",
+		},
+		{
+			name: "Test Square Root of 64",
+			request: 64,
+			expected: 8,
+			option: "sqrt",
+		},
+		{
+			name: "Test Round of 4.4",
+			request: 4.4,
+			expected: 4,
+			option: "round",
+		},
+	}
+
+	// iterate through tests array of struct
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// declare result
+			var result float64
+			// option check
+			switch test.option {
+			case "sqrt":
+				result = math_pkg.SquareRoot(test.request)
+			case "round":
+				result = math_pkg.Round(test.request)
+			}
+			// assertion
+			require.Equal(t, test.expected, result, fmt.Sprintf("expected %f but got %f", test.expected, result))
+			// print if success
+			fmt.Println("Table Test passed for:", test.name)
+		})
+	}
 }

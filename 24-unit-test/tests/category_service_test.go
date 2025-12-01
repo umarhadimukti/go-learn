9a6cf713122e83cba758bc68de13cac44b4a025f
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-12-01 11:56:39 +0700

    fix: implement get success mock test in category

--- 24-unit-test/tests/category_service_test.go
+++ 24-unit-test/tests/category_service_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/umarhadimukti/go-learn/24-unit-test/entities"
 	repository "github.com/umarhadimukti/go-learn/24-unit-test/repositories"
 	service "github.com/umarhadimukti/go-learn/24-unit-test/services"
 )
@@ -12,12 +13,25 @@ import (
 var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
 var categoryService = service.CategoryService{Repository: categoryRepository}
 
-func TestCategoryService_Get(t *testing.T) {
+func TestCategoryService_GetNotFound(t *testing.T) {
 	// mock program
 	categoryRepository.Mock.On("FindById", "2").Return(nil)
 
 	category, error := categoryService.GetDetail("2")
 	assert.Nil(t, category)
 	assert.NotNil(t, error)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	categoryMock := entities.Category{
+		Id: "1",
+		Name: "Sport",
+	}
+	// mock program
+	categoryRepository.Mock.On("FindById", "1").Return(categoryMock)
+
+	result, err := categoryService.GetDetail("1")
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
 }
 

dee60c0a59b3389baa8ff9bcfcbb7ab50ab3f540
Umar Hadi Mukti <umarhadimukti@gmail.com>
2026-01-06 13:28:45 +0700

    feat: add message interface and struct

--- 11-go-interface/main.go
+++ 11-go-interface/main.go
@@ -1,33 +1,51 @@
-package main;
+package main
 
-import "fmt";
+import (
+	"fmt"
+)
 
 type HasPosition interface {
-	GetPosition() string;
-	SetPosition(position string);
+	GetPosition() string
+	SetPosition(position string)
 }
 
 type Employee struct {
-	Name, Position, Country string;
-	Age int;
+	Name, Position, Country string
+	Age                     int
 }
 
 func (employee Employee) GetPosition() string {
-	return fmt.Sprint(employee.Name) + " is a " + fmt.Sprint(employee.Position);
+	return fmt.Sprint(employee.Name) + " is a " + fmt.Sprint(employee.Position)
 }
 
 func (employee Employee) SetPosition(position string) {
-	employee.Position = position;
-	Greeting(employee);
+	employee.Position = position
+	Greeting(employee)
 }
 
 func Greeting(employee HasPosition) {
-	fmt.Println("Greetings! " + employee.GetPosition());
+	fmt.Println("Greetings! " + employee.GetPosition())
+}
+
+type Message struct {
+	ID                        int
+	SenderName, Type, Content string
+}
+
+func ShowMessageType(message Message) {
+	fmt.Println("Message ID:", message.ID)
+	fmt.Println("Sender:", message.SenderName)
+	fmt.Println("Type:", message.Type)
+	fmt.Println("Content:", message.Content)
 }
 
 func main() {
-	firstEmployee := Employee{"Umar", "Software Engineer", "Indonesia", 14};
-	Greeting(firstEmployee);
-	fmt.Println("=======================================");
-	firstEmployee.SetPosition("Senior Software Engineer");
+	firstEmployee := Employee{"Umar", "Software Engineer", "Indonesia", 14}
+	Greeting(firstEmployee)
+	firstEmployee.SetPosition("Senior Software Engineer")
+	fmt.Println("=======================================")
+
+	firstMessage := Message{1, "Umar Hadi", "private", "Hello, sir"}
+	ShowMessageType(firstMessage)
+	fmt.Println("=======================================")
 }

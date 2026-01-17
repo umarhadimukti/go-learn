a1b0b9124fb73440d9df0ab1b7a59b510984a4be
Umar Hadi Mukti <umarhadimukti@gmail.com>
2026-01-17 20:33:35 +0700

    feat: refactor counter retrieval to use atomic load in getCounter function

--- go-atomic/main.go
+++ go-atomic/main.go
@@ -6,12 +6,21 @@ import (
 	"sync/atomic"
 )
 
+/* atomic can process only single variable without mutex
+	* if there is many variable, its better to use mutex than atomic
+*/
+
 var wg sync.WaitGroup
 var counter int64
 
 func incrementPrimitive() {
 	atomic.AddInt64(&counter, 1)
 	// counter++
+}
+
+func getCounter() {
+	result := atomic.LoadInt64(&counter)
+	fmt.Println("Counter with atomic:", result)
 }
 
 func main() {
@@ -23,5 +32,5 @@ func main() {
 		}()
 	}
 	wg.Wait()
-	fmt.Println("Counter with atomic:", counter)
+	getCounter()
 }
